c478ccc971cac14717ea6c61f6ac2dbb148b0981
Lucas <lucas.vale403@gmail.com>
2026-03-16 20:39:01 -0300

    loading3

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ referenciaDiv.classList.remove('visivel')
 loading.classList.add('ativo')
 setTimeout(() => {
     loading.classList.remove('ativo')
-}, 3000)
+}, 2500)
 
 // carregar vozes corretamente no celular
 function carregarVozes() {
